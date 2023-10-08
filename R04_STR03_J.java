d065a037e6ac85bd345b4f46b0e3a50a2465d5cf
Ayden <aydenlalond@gmail.com>
2023-10-08 11:55:48 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,7 +1,6 @@
 public class R04_STR03_J {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
+        String s = x.toString();
     }
 }
