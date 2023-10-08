b4cf960dca3fe3a7a6d8559691778f0bd0827ff0
Ayden <aydenlalond@gmail.com>
2023-10-08 00:08:38 -0400

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,8 @@
+// IDS03-J
+public class R00_IDS03_J{
+    public static void main(String[] args) {
+        boolean loginSuccessful = true;
+        String username = "admin";
+        System.out.println("User login " + (loginSuccessful ? "succeeded" : "failed") + " for: " + username);
+    }
+}
