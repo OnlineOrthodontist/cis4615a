48c81771a2c38ca20fed687fb856825a26dfcd7a
Ayden <aydenlalond@gmail.com>
2023-10-08 11:56:53 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,15 +1,22 @@
 public class R06_MET01_J {
-    public static void main(String[] args) {
-        int result = getAbsAdd(Integer.MIN_VALUE, 1);
-        System.out.println("Result: " + result);
+   public static void main(String[] args) {
+        try {
+            int result = getAbsAdd(Integer.MIN_VALUE, 1);
+            System.out.println("Result: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid input");
+        }
     }
 
     public static int getAbsAdd(int x, int y) {
-        assert x != Integer.MIN_VALUE;
-        assert y != Integer.MIN_VALUE;
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
         int absX = Math.abs(x);
         int absY = Math.abs(y);
-        assert (absX <= Integer.MAX_VALUE - absY);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
         return absX + absY;
     }
 }
