779052f4aa4933465d8c7627f09bad960af2a240
Ayden <aydenlalond@gmail.com>
2023-10-08 12:03:14 -0400

    Update R49_MSC02_J.java

--- R49_MSC02_J.java
+++ R49_MSC02_J.java
@@ -1,6 +1,7 @@
 public class R49_MSC02_J {
     public static void main(String[] args) {
-        int x = (int) (Math.random() * 1000000);
+        SecureRandom rand = new SecureRandom();
+        int x = rand.nextInt(1000000);
         System.out.println("Random number: " + x);
     }
 }
